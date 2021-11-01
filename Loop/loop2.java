9521623b765dfa832ff3886d3be618d12dd0ff5f
HseongH <hh4518@gmail.com>
2021-11-01 23:57:06 +0900

    JAVA 반복문 연습

---
+++ Loop/loop2.java
@@ -0,0 +1,12 @@
+
+public class loop2 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+		
+		for (int j = 0; j <= 5; j++) {
+			System.out.println(j);
+		}
+	}
+}
---
+++ Loop/loop4.java
@@ -0,0 +1,12 @@
+
+public class loop4 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		while (sum <= 100) {
+			System.out.printf("%d - %d%n", i, sum);
+			sum += ++i;
+		}
+	}
+}
