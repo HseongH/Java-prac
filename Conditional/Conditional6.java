9d68225b8822024f3be3e726df55cf05d3099c9b
HseongH <hh4518@gmail.com>
2021-11-01 11:48:20 +0900

    JAVA 기본 조건문 연습

---
+++ Conditional/Conditional6.java
@@ -0,0 +1,11 @@
+
+public class Conditional6 {
+	public static void main(String[] args) {
+		int num = 0;
+		
+		for (int i = 0; i < 6; i++) {
+			num = (int) (Math.random() * 6) + 1;
+			System.out.println(num);
+		}
+	}
+}
